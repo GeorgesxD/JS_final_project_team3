5ead60a72b501afe33dbb1e3536822c41a824926
mo <mo@gmail.com>
2024-05-12 15:31:19 +0300

    initial commit

--- JS.js
+++ JS.js
@@ -1,1 +1,26 @@
+let startBtn = document.querySelector(".startBtn");
+let guideCard = document.querySelector(".guide-card");
+let exitBtn = document.querySelector(".exitBtn");
+let playBtn = document.querySelector(".playBtn");
+let mainHome = document.querySelector(".main-home");
+let playPage = document.querySelector(".play-page");
 
+startBtn.addEventListener("click", () => {
+  guideCard.classList.remove("active");
+  mainHome.classList.add("blur");
+});
+
+exitBtn.addEventListener("click", () => {
+  guideCard.classList.add("active");
+  mainHome.classList.remove("blur");
+});
+
+playBtn.addEventListener("click", () => {
+  mainHome.classList.remove("blur");
+  guideCard.classList.add("active");
+
+  setTimeout(() => {
+    mainHome.style.left = 0;
+    playPage.style.left = 0;
+  }, 1000);
+});
