6c57fab2dd64c92277cfb3fcd21b70637e7d6e77
GeorgesxD <148150023+GeorgesxD@users.noreply.github.com>
2024-05-14 22:19:34 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,4 +1,4 @@
-// Selecting DOM elements
+\// Selecting DOM elements
 let startBtn = document.querySelector(".startBtn");
 let guideCard = document.querySelector(".guide-card");
 let exitBtn = document.querySelector(".exitBtn");
@@ -40,19 +40,42 @@ playBtn.addEventListener("click", () => {
   }, 2000);
 });
 
-let qa = {"Java":'System.out.println("Hello World!")',"Python":'print("Hello World!)',"Javascript":"","C++":"","C#":"","GO":"","Rust":"","Pascal":"","Swift":"","php":""};
+let qa = {"Java":'System.out.println("Hello World!")',"Python":'print("Hello World!")',"Javascript":'console.log("Hello World!")',"C++":'cout << "Hello World!"',"C#":'Console.WriteLine("Hello World!")',"GO":'fmt.Println("Hello World!")',"Rust":'println!("Hello World!")',"Pascal":"writeln('Hello World!')","Swift":'print("Hello World!")',"php":'echo "Hello World!"'};
 let keys = ["Java","Python","Javascript","C++","C#","GO","Rust","Pascal","Swift","php"];
 let i=0;
+let wrong = 0;
 
 function checkAnswer(){
   let answer = document.getElementById("answer").value;
-    if(qa[keys[i]]===answer){
+    if(i>=9){
+      if(qa[keys[9]]===answer){
+        alert("Good Job!");
+      }
+      else{
+        alert(" Incorrect, the correct answer was: "+qa[keys[9]]);
+      }
+      
+      document.getElementById("q").innerHTML = 'Your Results'
+      document.getElementById("counter").innerHTML = "10 out of 10";
+      document.getElementById("answer-texts").innerHTML = "Your final score is: "+(10-wrong)+"/10 !";
+      document.getElementById("answer").value="";
+    }
+    else if(qa[keys[i]]===answer && i<9){
       alert("Good Job!");
       i++;
+      document.getElementById("q").innerHTML ='Do you Know How to Write " Hello World! " in '+keys[i]+" ?";
+      document.getElementById("counter").innerHTML = (i+1)+" out of 10";
+      document.getElementById("answer").value="";
     }
-    else{
+    else {
       alert(" Incorrect, the correct answer was: "+qa[keys[i]]);
-      
+      i++;
+      document.getElementById("q").innerHTML ='Do you Know How to Write " Hello World! " in '+keys[i]+" ?";
+      document.getElementById("counter").innerHTML = (i+1)+" out of 10";
+      document.getElementById("answer").value="";
+      wrong++;
     }
-  };
+ };
+
+ 
 
