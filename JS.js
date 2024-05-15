5036a01a905789a844e1141de0b755890f9076b6
GeorgesxD <148150023+GeorgesxD@users.noreply.github.com>
2024-05-15 20:48:17 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,4 +1,4 @@
-// Selecting DOM elements
+
 let startBtn = document.querySelector(".startBtn");
 let guideCard = document.querySelector(".guide-card");
 let exitBtn = document.querySelector(".exitBtn");
@@ -8,27 +8,27 @@ let playPage = document.querySelector(".play-page");
 let playPageTitle = document.querySelector(".play-page-title");
 let playCard = document.querySelector(".play-card");
 
-// Event listener for the start button
+
 startBtn.addEventListener("click", () => {
   guideCard.classList.remove("active");
 
   mainHome.classList.add("blur");
 });
 
-// Event listener for the exit button
+
 exitBtn.addEventListener("click", () => {
   guideCard.classList.add("active");
 
   mainHome.classList.remove("blur");
 });
 
-// Event listener for the play button
+
 playBtn.addEventListener("click", () => {
   mainHome.classList.remove("blur");
 
   guideCard.classList.add("active");
 
-  // Setting timeout for animations
+
   setTimeout(() => {
     mainHome.style.left = 0;
     playPage.style.left = 0;
